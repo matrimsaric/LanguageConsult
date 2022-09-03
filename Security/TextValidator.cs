550f485fdd79f3d7eb942f6ccf364fbb3a4d656a
matri <matrimsaric@hotmail.co.uk>
2022-09-03 18:55:27 +0100

    Adjust tag from Consult files.

---
+++ Security/TextValidator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LanguageConsult.Security
+{
+    internal class TextValidator
+    {
+        private Regex checkString = new Regex(@"^[a-zA-Z]+$");//^[a-zA-Z]+$  [\\s\\w\\.]*
+        private Regex checkHiragana = new Regex(@"^[\u3040-\u309Fー]+$");
+         internal bool ValidateText(string unsafeText, out string safeText)
+        {
+            bool passedSearch = false;
+            safeText = String.Empty;
+
+            // space is permitted but is an extra char
+            if (!checkString.IsMatch(unsafeText.Replace(" ", String.Empty)))
+            {
+                safeText = unsafeText.Replace(checkString.ToString(),String.Empty);
+            }
+            else
+            {
+                // passed
+                safeText = unsafeText;// only time this it permitted
+                passedSearch = true;
+            }
+
+            return passedSearch;
+
+
+        }
+
+        internal bool ValidateKanji(string unsafeKanji, out string safeKanji)
+        {
+            bool passedSearch = false;
+            safeKanji = String.Empty;
+
+            // looking for incidence of any standard text
+            if (checkString.IsMatch(unsafeKanji))
+            {
+                safeKanji = unsafeKanji.Replace(checkString.ToString(), String.Empty);
+            }
+            else
+            {
+                // passed
+                safeKanji = unsafeKanji;// only time this it permitted
+                passedSearch = true;
+            }
+
+            return passedSearch;
+
+
+        }
+
+        internal bool ValidateHiragana(string unsafeHirgana, out string safeHiragana)
+        {
+            bool passedSearch = false;
+            safeHiragana = String.Empty;
+
+            if (!checkHiragana.IsMatch(unsafeHirgana))
+            {
+                safeHiragana = unsafeHirgana.Replace(checkString.ToString(), String.Empty);
+            }
+            else
+            {
+                // passed
+                safeHiragana = unsafeHirgana;// only time this it permitted
+                passedSearch = true;
+            }
+
+            return passedSearch;
+
+
+        }
+
+
+    }
+}
--- Verbs/Tense.cs
+++ Verbs/Tense.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LanguageConsult.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,27 +20,96 @@ namespace LanguageConsult.Verbs
         OTHER = 7
 
     }
+
+    internal enum LANGUAGE_TYPE
+    {
+        ENGLISH = 0,
+        HIRAGANA = 1,
+        KANJI = 2
+    }
     public class Tense
     {
+        private TextValidator textValidator = new TextValidator();
         public bool Polite { get; internal protected set; }
         public TENSE_TYPE tenseType = TENSE_TYPE.UNKNOWN;
 
-        public string Kanji { get; set; }
-        public string Hiragana { get; set; }
-        public string Romaji { get; set; }
-        public string Meaning { get; set; }
-        public string Notes { get; set; }
+        public string Kanji { get; internal protected set; }
+        public string Hiragana { get; internal protected set; }
+        public string Romaji { get; internal protected set; }
+        public string Meaning { get; internal protected set; }
+        public string Notes { get; internal protected set; }
 
         internal Guid Id { get; set; }
 
-        public Tense(string kanji, string hiragana, string romaji, string meaning, string notes, Guid guid)
-        { 
-            Kanji = kanji;
-            Hiragana = hiragana;
-            Romaji = romaji;
-            Meaning = meaning;
-            Notes = notes;
+        public Tense(string unsafeKanji, string unsafeHiragana, string unsafeRomaji, string unsafeMeaning, string unsafeNotes,
+            Guid guid, TENSE_TYPE setTenseType, bool polite = false)
+        {
+            
+            if(guid == Guid.Empty)
+            {
+                throw new ArgumentException(nameof(guid));
+            }
+            if(setTenseType == TENSE_TYPE.UNKNOWN)
+            {
+                throw new ArgumentException(nameof(setTenseType));
+            }
+            Kanji = GetSafeString(unsafeKanji, "Kanji", languageType: LANGUAGE_TYPE.KANJI);
+            Hiragana = GetSafeString(unsafeHiragana, "Hiragana", languageType: LANGUAGE_TYPE.HIRAGANA);
+            Romaji = GetSafeString(unsafeRomaji, "Romaji");
+            Meaning = GetSafeString(unsafeMeaning, "Meaning");
+            Notes = GetSafeString(unsafeNotes, "Notes", languageType: LANGUAGE_TYPE.ENGLISH, checkForEmpty: false);
             Id = guid;
+            Polite = polite;
+            tenseType = setTenseType;
+        }
+
+        private string GetSafeString(string unsafeValue, string namingType, LANGUAGE_TYPE languageType = LANGUAGE_TYPE.ENGLISH,   bool checkForEmpty = true)
+        {
+
+
+            if (checkForEmpty == true)
+            {
+                if (string.IsNullOrWhiteSpace(unsafeValue))
+                {
+                    throw new ArgumentNullException(nameof(namingType));
+                }
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(unsafeValue))
+                {
+                    // dont care about empty for this field so pass the check and carry on
+                    return String.Empty;
+                }
+
+            }
+
+
+            string safeValue = String.Empty;
+            bool passedTest = false;
+
+            switch (languageType)
+            {
+                case LANGUAGE_TYPE.HIRAGANA:
+                    passedTest = textValidator.ValidateHiragana(unsafeValue, out safeValue);
+                    break;
+                case LANGUAGE_TYPE.KANJI:
+                    passedTest = textValidator.ValidateKanji(unsafeValue, out safeValue);
+                    break;
+                default:
+                    passedTest = textValidator.ValidateText(unsafeValue, out safeValue);
+                    break;
+            }
+            
+
+
+            // security check
+            if (!passedTest)
+            {
+                throw new AuthenticationException(namingType);
+            }
+
+            return safeValue;
         }
     }
 }
